378860a7fcdd9c010ebc21e985fc085aa2bd6cc3
gzerad <dfedoseyev@gmail.com>
2021-08-30 13:47:22 +0300

    M92 Release

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+    name: "WebRTC",    
+    products: [
+        .library(
+            name: "WebRTC",
+            targets: ["WebRTC"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "WebRTC", 
+            url: "https://github.com/100mslive/webrtc-ios/releases/download/m92/WebRTC.xcframework.zip",
+            checksum: "0fce1d7a126034864640ab350c73bc359bf7c0dc6572af7cf0f6246e1106dc74"
+        )        
+    ]
+)

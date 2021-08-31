abe74fd2aa727de759e6466ad22c3419cdf2d7a4
gzerad <dfedoseyev@gmail.com>
2021-08-31 13:38:25 +0300

    Add license to archive

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .binaryTarget(
             name: "WebRTC", 
             url: "https://github.com/100mslive/webrtc-ios/releases/download/1.0.4515/WebRTC.xcframework.zip",
-            checksum: "0fce1d7a126034864640ab350c73bc359bf7c0dc6572af7cf0f6246e1106dc74"
+            checksum: "4817c0985e66472230df9703fc6b79a63da8a2f34134d78a0b759462ec99154f"
         )        
     ]
 )

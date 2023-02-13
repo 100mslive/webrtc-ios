8544adb60585404ff1cb5798238309a46792845f
gzerad <dfedoseyev@gmail.com>
2023-02-13 12:50:54 +0200

    1.0.5114 Release

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "WebRTC", 
-            url: "https://github.com/100mslive/webrtc-ios/releases/download/1.0.5113/WebRTC.xcframework.zip",
+            url: "https://github.com/100mslive/webrtc-ios/releases/download/1.0.5114/WebRTC.xcframework.zip",
             checksum: "ab9c595207d29de457c22b2ec3fa7e632ef57daf9fbc90ed93eaf99a64b1893f"
         )        
     ]
